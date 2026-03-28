91249cf28db7127260613e3bfadea5b6139a0783
mjaafar91 <jaffer12333@hotmail.com>
2026-03-28 23:14:30 +0300

    commit all

---
+++ RGBLed/RGBLed.ino
@@ -0,0 +1,33 @@
+const int redPin = 9;
+const int greenPin = 10;
+const int bluePin = 11;
+
+void setup() {
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+}
+
+// helper function to set color
+void setColor(int red, int green, int blue) {
+  analogWrite(redPin, red);
+  analogWrite(greenPin, green);
+  analogWrite(bluePin, blue);
+}
+
+void loop() {
+  setColor(255, 0, 0);   // Red
+  delay(1000);
+  setColor(0, 255, 0);   // Green
+  delay(1000);
+  setColor(0, 0, 255);   // Blue
+  delay(1000);
+  setColor(255, 255, 0); // Yellow
+  delay(1000);
+  setColor(0, 255, 255); // Cyan
+  delay(1000);
+  setColor(255, 0, 255); // Magenta
+  delay(1000);
+  setColor(255, 255, 255); // White
+  delay(1000);
+}
